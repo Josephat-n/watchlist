f2090ccce9ff281fd66240a7a9f85f3f34cbb405
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-24 14:51:32 +0300

    add start.sh in .gitignore

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MOVIE_API_KEY='e4869686b2dcffbe18375f1583f1650b'
-export SECRET_KEY='josephatNJOROGE@123456'
-export MAIL_USERNAME=josphatmax@gmail.com 
-export MAIL_PASSWORD=josegN@12
-
-python3.6 manage.py server
